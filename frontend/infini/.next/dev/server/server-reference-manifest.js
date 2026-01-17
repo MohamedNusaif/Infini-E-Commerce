7f43e5b45e5a4951e7229bc6b3ea48a2d5b75909
MohamedNusaif <mrmnusaif@gmail.com>
2026-01-17 21:50:20 +0530

    add infini frontend

--- frontend/infini/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nVRrmIbP8YynfspWet0FKYFmQNICCvTCwVMX/WjIazc=\"\n}"
--- frontend/infini/.next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_globals_71f961d1.css",
-  "static/chunks/_bc2c2a63._.js"
-],
-    source: "dynamic"
-});
